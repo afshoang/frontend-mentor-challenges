912c7a5271330ec68db0171c176fc020ec4e7a99
afshoang <kaitokid19xx@gmail.com>
2023-11-05 15:27:05 +0700

    solution for news homepage

--- news-homepage-main/js/script.js
+++ news-homepage-main/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hoang🎉🎉🎉🎉");
+const openNavbarMobileBtn = document.querySelector('svg.icon-menu')
+const closeNavbarMobileBtn = document.querySelector('.nav-mobile svg')
+const navBarMobile = document.querySelector('.nav-mobile')
+const overlay = document.querySelector('.overlay')
+
+openNavbarMobileBtn.addEventListener('click', () => {
+    navBarMobile.classList.add('active')
+    overlay.classList.add('active')
+})
+
+navBarMobile.addEventListener('click', () => {
+    navBarMobile.classList.remove('active')
+    overlay.classList.remove('active')
+})
+
+
