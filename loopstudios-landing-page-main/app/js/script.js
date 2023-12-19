21c472b3c60e2462a3af8e788034c1e99f92b573
afshoang <kaitokid19xx@gmail.com>
2023-12-19 16:53:07 +0700

    desktop layout

--- loopstudios-landing-page-main/app/js/script.js
+++ loopstudios-landing-page-main/app/js/script.js
@@ -2,6 +2,14 @@ const header = document.querySelector('#header')
 const headerToggleBtn = document.querySelector('.header__toggle')
 const headerCloseBtn = document.querySelector('.header__close')
 
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        header.classList.add('shrink')
+    } else {
+        header.classList.remove('shrink')
+    }
+})
+
 headerToggleBtn.addEventListener('click', () => {
     header.classList.add('active')
 })
